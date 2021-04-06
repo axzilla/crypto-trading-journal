4615284dd0ccf1c33753b1a760ac27cc1dccbbc3
Axel <mail@badazz.dev>
2021-04-06 10:48:51 +0200

    Update production DB env 5

--- knex/knex.js
+++ knex/knex.js
@@ -1,7 +1,4 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-console.log('-----------------------------------------') // eslint-disable-line
-console.log(process.env.ENVIRONMENT) // eslint-disable-line
-console.log('-----------------------------------------') // eslint-disable-line
 const environment = process.env.ENVIRONMENT || 'development'
 const config = require('../knexfile.js')[environment]
 module.exports = require('knex')(config)
