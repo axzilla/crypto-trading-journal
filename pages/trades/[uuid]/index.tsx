5e89117a6e01465587a8125b5ccd3a7d5e64cfa3
‘axzilla’ <chanter-acidic-0r@icloud.com>
2021-04-06 20:10:16 +0200

    Improve UI on trades overview

--- pages/trades/[uuid]/index.tsx
+++ pages/trades/[uuid]/index.tsx
@@ -1,12 +1,32 @@
-import React from 'react'
+// Packages
 import PropTypes from 'prop-types'
 import { getSession } from 'next-auth/client'
 import { GetServerSideProps } from 'next'
 import { useRouter } from 'next/router'
 import useSWR from 'swr'
 import { request } from 'graphql-request'
+import moment from 'moment'
 
+// Geist UI
+import { GeistUIThemes, Table, Tag, Text, Card } from '@geist-ui/react'
+
+// Utils
+import makeStyles from '../../../utils/makeStyles'
+
+// Layouts
 import { App as AppLayout } from './../../../layouts'
+
+const useStyles = makeStyles((ui: GeistUIThemes) => ({
+  content: {
+    display: 'flex',
+    flexDirection: 'row',
+    width: ui.layout.pageWidthWithMargin,
+    maxWidth: '100%',
+    padding: `calc(${ui.layout.gap} * 2) ${ui.layout.pageMargin} calc(${ui.layout.gap} * 2)`,
+    boxSizing: 'border-box',
+    margin: '0 auto'
+  }
+}))
 
 type DashboardProps = {
   toggleDarkMode: () => void
@@ -33,6 +53,7 @@ const TRADE_BY_UUID_QUERY = /* GraphQL */ `
 `
 
 function Dashboard({ toggleDarkMode, themeType }: DashboardProps): JSX.Element {
+  const classes = useStyles()
   const router = useRouter()
   const { uuid } = router.query
 
@@ -40,12 +61,60 @@ function Dashboard({ toggleDarkMode, themeType }: DashboardProps): JSX.Element {
     request('/api/graphql', query, { uuid })
   )
 
-if (error) console.log('failed to load') // eslint-disable-line
-if (!data) console.log('loading') // eslint-disable-line
+  if (error) console.log('failed to load') // eslint-disable-line
+  if (!data) console.log('loading') // eslint-disable-line
+  if (data) console.log(data.tradeByUuid) // eslint-disable-line
 
-  return (
+  return !data ? (
+    <p>Loading</p>
+  ) : (
     <AppLayout toggleDarkMode={toggleDarkMode} themeType={themeType}>
-      <p>{uuid}</p>
+      <div className={classes.content}>
+        <Card>
+          <Text h4>
+            {data.tradeByUuid[0].symbol}:{data.tradeByUuid[0].exchange}
+          </Text>
+          <div style={{ overflow: 'scroll' }}>
+            <Table
+              data={
+                data &&
+                data.tradeByUuid
+                  .sort((a, b) => b.date_created - a.date_created)
+                  .map(trade => {
+                    const { action, price, quantity, fee, date, status } = trade
+
+                    return {
+                      action: <Tag type="warning">{action.toUpperCase()}</Tag>,
+                      price,
+                      quantity,
+                      fee,
+                      date: (
+                        <div style={{ display: 'flex', whiteSpace: 'nowrap' }}>
+                          {moment
+                            .unix(date / 1000)
+                            .utc()
+                            .format('MMMM D, YYYY, h:mm')}
+                        </div>
+                      ),
+                      status: (
+                        <Tag invert type="warning">
+                          {status.toUpperCase()}
+                        </Tag>
+                      )
+                    }
+                  })
+              }
+            >
+              <Table.Column prop="action" label="action" />
+              <Table.Column prop="price" label="price" />
+              <Table.Column prop="quantity" label="quantity" />
+              <Table.Column prop="fee" label="fee" />
+              <Table.Column prop="date" label="date" />
+              <Table.Column prop="status" label="status" />
+            </Table>
+          </div>
+        </Card>
+      </div>
     </AppLayout>
   )
 }
