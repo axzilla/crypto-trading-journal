fb9dd931400349f9b75cfa3ffe36d0bef810f5c4
Axel <mail@badazz.dev>
2021-04-06 08:58:01 +0200

    Use .env in knexfile

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ require('dotenv').config({ path: './.env.local' })
 module.exports = {
   development: {
     client: 'pg',
-    connection: 'postgres://postgres:5080@127.0.0.1:5432/crypto-trading-tools?synchronize=true',
+    connection: process.env.DATABASE_URL,
     migrations: { directory: __dirname + '/knex/migrations' },
     seeds: { directory: __dirname + '/knex/seeds' }
   }
