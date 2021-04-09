86fa45742d19c8f56c9a3d0496ea5b1952b10203
Axel <mail@badazz.dev>
2021-04-09 18:31:55 +0200

    Update next-auth db settings

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -28,7 +28,9 @@ export default NextAuth({
     port: process.env.DATABASE_PORT,
     username: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME
+    database: process.env.DATABASE_NAME,
+    ssl: true,
+    extra: { ssl: { rejectUnauthorized: false } }
   },
   callbacks: {
     jwt: async (token, user) => {
