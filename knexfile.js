c0e6d4391d1c8d2a7cba23bc6a2c4bd81bfd89c4
Axel Adrian <mail@badazz.dev>
2021-04-09 16:20:55 +0000

    Resolve "Fix Vercel issues"

--- knexfile.js
+++ knexfile.js
@@ -20,7 +20,11 @@ module.exports = {
       user: process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE_NAME,
-      port: process.env.DATABASE_PORT
+      port: process.env.DATABASE_PORT,
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
     },
     migrations: { directory: __dirname + '/knex/migrations' }
   }
