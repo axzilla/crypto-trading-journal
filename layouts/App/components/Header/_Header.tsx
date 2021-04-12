8a9f062408da0ae28c3e2de9fef033bedb26bbb5
‘axzilla’ <chanter-acidic-0r@icloud.com>
2021-04-12 17:32:29 +0200

    Create readme, update _Header, update package.json

--- layouts/App/components/Header/_Header.tsx
+++ layouts/App/components/Header/_Header.tsx
@@ -7,7 +7,7 @@ import { signOut, useSession } from 'next-auth/client'
 import makeStyles from 'utils/makeStyles'
 
 // Geist UI
-import { Button, GeistUIThemes, Link, Popover, Tabs, Avatar, Spacer } from '@geist-ui/react'
+import { Button, GeistUIThemes, Link, Popover, Avatar, Spacer, Grid, Text } from '@geist-ui/react'
 import {
   Sun as SunIcon,
   Moon as MoonIcon,
@@ -15,6 +15,7 @@ import {
 } from '@geist-ui/react-icons'
 
 const useStyles = makeStyles((ui: GeistUIThemes) => ({
+  root: { borderBottom: `solid 1px ${ui.palette.accents_2}` },
   header: {
     width: ui.layout.pageWidthWithMargin,
     maxWidth: '100%',
@@ -25,68 +26,11 @@ const useStyles = makeStyles((ui: GeistUIThemes) => ({
   },
   headerContent: {
     height: '100%',
+    width: '100%',
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'space-between',
     padding: `0 ${ui.layout.pageMargin}`
-  },
-  headerTitle: {
-    fontWeight: 500,
-    display: 'flex',
-    alignItems: 'center',
-    paddingLeft: 10
-  },
-  nav: {
-    position: 'sticky',
-    top: 0,
-    borderBottom: `solid 1px ${ui.palette.accents_2}`,
-    zIndex: 15
-  },
-  navFixed: {
-    borderBottom: ui.type === 'light' && 'none',
-    boxShadow: ui.type === 'light' && 'rgba(0, 0, 0, 0.1) 0 0 15px 0'
-  },
-  navContent: {
-    width: ui.layout.pageWidthWithMargin,
-    maxWidth: '100%',
-    height: '100%',
-    margin: '0 auto',
-    '& .tabs header': {
-      padding: `0 ${ui.layout.pageMargin}`,
-      borderBottom: 'none !important',
-      flexWrap: 'nowrap !important',
-      overflowY: 'hidden',
-      overflowX: 'auto',
-      overflow: '-moz-scrollbars-none',
-      '-ms-overflow-style': 'none',
-      '&::-webkit-scrollbar': {
-        display: 'none'
-      }
-    },
-    '& .content': {
-      display: 'none !important'
-    },
-    '& .tab': {
-      padding: '12px 0 !important',
-      // margin: '0 !important',
-      fontSize: '14px !important'
-    }
-  },
-  sidebar: {
-    display: 'flex',
-    alignItems: 'center !important'
-  },
-  themeIcon: {
-    width: '40px !important',
-    height: '40px !important',
-    display: 'flex !important',
-    justifyContent: 'center !important',
-    alignItems: 'center !important',
-    marginRight: 5,
-    padding: '0 !important'
-  },
-  popover: {
-    width: '180px !important'
   }
 }))
 
@@ -106,48 +50,38 @@ function Menu({ toggleDarkMode, themeType }: Props): JSX.Element {
   }, [fixed])
 
   return (
-    <>
-      <div className={classes.header}>
-        <div className={classes.headerContent}>
-          <div style={{ display: 'flex' }}>
+    <Grid className={classes.root}>
+      <Grid.Container className={classes.header}>
+        <Grid className={classes.headerContent} justify="space-between">
+          <Grid.Container alignItems="center">
             <TrendingUpIcon />
-            <div className={classes.headerTitle}>Crypto Trading Tools</div>
-          </div>
-          <div className={classes.sidebar}>
-            <Popover
-              content={
-                <>
-                  <Popover.Item title>
-                    <span>{session.user.email}</span>
-                  </Popover.Item>
-                  <Popover.Item onClick={signOut}>
-                    <Link>Logout</Link>
-                  </Popover.Item>
-                </>
-              }
-              placement="bottomEnd"
-              portalClassName={classes.popover}
-            >
-              <Avatar text={session.user.email.slice(0, 1).toUpperCase()} />
-            </Popover>
-            <Spacer />
-            <Button
-              onClick={toggleDarkMode}
-              iconRight={themeType === 'dark' ? <MoonIcon /> : <SunIcon />}
-              auto
-              size="small"
-            />
-          </div>
-        </div>
-      </div>
-      <nav className={classes.nav + ' ' + (fixed ? classes.navFixed : '')}>
-        <div className={classes.navContent}>
-          <Tabs initialValue="1">
-            <Tabs.Item label="Trading Journal" value="1" />
-          </Tabs>
-        </div>
-      </nav>
-    </>
+            <Spacer x={0.5} />
+            <Text className={classes.headerTitle}>Crypto Trading Tools</Text>
+          </Grid.Container>
+
+          <Popover
+            content={
+              <>
+                <Popover.Item title>{session.user.email}</Popover.Item>
+                <Popover.Item onClick={signOut}>
+                  <Link>Logout</Link>
+                </Popover.Item>
+              </>
+            }
+            placement="bottomEnd"
+          >
+            <Avatar text={session.user.email.slice(0, 1).toUpperCase()} />
+          </Popover>
+          <Spacer />
+          <Button
+            onClick={toggleDarkMode}
+            iconRight={themeType === 'dark' ? <MoonIcon /> : <SunIcon />}
+            auto
+            size="small"
+          />
+        </Grid>
+      </Grid.Container>
+    </Grid>
   )
 }
 
