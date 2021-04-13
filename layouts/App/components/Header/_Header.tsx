114fe8e8cf59d7f0ec41981d40c1dee66521e140
‘axzilla’ <chanter-acidic-0r@icloud.com>
2021-04-13 23:10:52 +0200

    Finish branch (#4)

--- layouts/App/components/Header/_Header.tsx
+++ layouts/App/components/Header/_Header.tsx
@@ -1,18 +1,31 @@
 // Packages
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { signOut, useSession } from 'next-auth/client'
+import axios from 'axios'
 
 // Utils
 import makeStyles from 'utils/makeStyles'
 
 // Geist UI
-import { Button, GeistUIThemes, Link, Popover, Avatar, Spacer, Grid } from '@geist-ui/react'
+import {
+  Button,
+  GeistUIThemes,
+  Link,
+  Popover,
+  Avatar,
+  Spacer,
+  Grid,
+  Modal,
+  Textarea,
+  useToasts
+} from '@geist-ui/react'
 import {
   Sun as SunIcon,
   Moon as MoonIcon,
   TrendingUp as TrendingUpIcon,
-  Github as GithubIcon
+  Github as GithubIcon,
+  Mail as MailIcon
 } from '@geist-ui/react-icons'
 
 const useStyles = makeStyles((ui: GeistUIThemes) => ({
@@ -37,58 +50,88 @@ const useStyles = makeStyles((ui: GeistUIThemes) => ({
 
 function Menu({ toggleDarkMode, themeType }: Props): JSX.Element {
   const classes = useStyles()
-  const [fixed, setFixed] = useState(false)
   const [session] = useSession()
+  const [, setToast] = useToasts()
+  const [isFeedbackModalOpen, setIsFeedbackModalOpen] = useState(false)
+  const [feedback, setFeedback] = useState('')
 
-  useEffect(() => {
-    const scrollHandler = () => {
-      const shouldFixed = document.documentElement.scrollTop > 60
-      if (fixed !== shouldFixed) setFixed(shouldFixed)
+  async function handleSendFeedback() {
+    try {
+      await axios.post('/api/feedback', { feedback })
+      setIsFeedbackModalOpen(false)
+      setFeedback('')
+      setToast({ text: 'Thanks for your feedback!', type: 'success', delay: 5000 })
+    } catch (error) {
+      setToast({ text: 'Error, please try it again!', type: 'error', delay: 5000 })
     }
-
-    document.addEventListener('scroll', scrollHandler)
-    return () => document.removeEventListener('scroll', scrollHandler)
-  }, [fixed])
+  }
 
   return (
-    <Grid className={classes.root}>
-      <Grid.Container className={classes.header}>
-        <Grid className={classes.headerContent} justify="space-between">
-          <Grid.Container alignItems="center">
-            <TrendingUpIcon />
-          </Grid.Container>
+    <>
+      <Grid className={classes.root}>
+        <Grid.Container className={classes.header}>
+          <Grid className={classes.headerContent} justify="space-between">
+            <Grid.Container alignItems="center">
+              <TrendingUpIcon />
+            </Grid.Container>
 
-          {session && (
-            <>
-              <Popover
-                content={
-                  <>
-                    <Popover.Item title>{session.user.email}</Popover.Item>
-                    <Popover.Item onClick={signOut}>
-                      <Link>Logout</Link>
-                    </Popover.Item>
-                  </>
-                }
-                placement="bottomEnd"
-              >
-                <Avatar text={session.user.email.slice(0, 1).toUpperCase()} />
-              </Popover>
-              <Spacer />
-            </>
-          )}
-          <Button
-            onClick={toggleDarkMode}
-            iconRight={themeType === 'dark' ? <MoonIcon /> : <SunIcon />}
-            auto
-            size="small"
+            {session && (
+              <>
+                <Popover
+                  content={
+                    <>
+                      <Popover.Item title>{session.user.email}</Popover.Item>
+                      <Popover.Item onClick={signOut}>
+                        <Link>Logout</Link>
+                      </Popover.Item>
+                    </>
+                  }
+                  placement="bottomEnd"
+                >
+                  <Avatar text={session.user.email.slice(0, 1).toUpperCase()} />
+                </Popover>
+                <Spacer />
+                <Button
+                  iconRight={<MailIcon />}
+                  onClick={() => setIsFeedbackModalOpen(true)}
+                  auto
+                  size="small"
+                />
+                <Spacer />
+              </>
+            )}
+            <Button
+              onClick={toggleDarkMode}
+              iconRight={themeType === 'dark' ? <MoonIcon /> : <SunIcon />}
+              auto
+              size="small"
+            />
+            <Spacer />
+            <Link href="https://github.com/badazzdev/crypto-trading-journal" target="_blank">
+              <GithubIcon size={20} />
+            </Link>
+          </Grid>
+        </Grid.Container>
+      </Grid>
+
+      <Modal open={isFeedbackModalOpen} onClose={() => setIsFeedbackModalOpen(false)}>
+        <Modal.Subtitle>Feedback</Modal.Subtitle>
+        <Modal.Content>
+          <Textarea
+            width="100%"
+            placeholder="Your feedback..."
+            value={feedback}
+            onChange={e => setFeedback(e.target.value)}
           />
-          <Spacer />
-          <Link href="https://github.com/badazzdev/crypto-trading-journal" target="_blank">
-            <GithubIcon size={20} />
-          </Link>
-        </Grid>
-      </Grid.Container>
-    </Grid>
+        </Modal.Content>
+        <Modal.Action passive onClick={() => setIsFeedbackModalOpen(false)}>
+          Cancel
+        </Modal.Action>
+        <Modal.Action disabled={!feedback} onClick={handleSendFeedback}>
+          Send
+        </Modal.Action>
+      </Modal>
+    </>
   )
 }
 
