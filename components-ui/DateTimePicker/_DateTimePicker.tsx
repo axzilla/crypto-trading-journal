10729b7807b64b966882c31b2d5ebf2cbc7ba0e1
Axel <mail@badazz.dev>
2021-04-16 18:46:20 +0200

    Fix issue on datetimepicker

--- components-ui/DateTimePicker/_DateTimePicker.tsx
+++ components-ui/DateTimePicker/_DateTimePicker.tsx
@@ -11,9 +11,50 @@ import makeStyles from 'utils/makeStyles'
 const useStyles = makeStyles((ui: GeistUIThemes) => ({
   label: { marginBottom: ui.layout.gapHalf },
   datePickerWrapper: {
+    '& .react-datepicker-popper': { zIndex: 999 },
+    '& .react-datepicker__month-container': {
+      background: ui.palette.background
+    },
+    '& .react-datepicker__day': {
+      color: ui.palette.accents_8,
+      '&:hover': {
+        borderRadius: '0.3rem',
+        backgroundColor: ui.palette.warning
+      }
+    },
+    '& .react-datepicker__day--selected': {
+      backgroundColor: ui.palette.warning
+    },
     '& .react-datepicker-wrapper': { width: '100%' },
     '& .react-datepicker__time-container': { overflow: 'hidden' },
-    '& .react-datepicker__time-list-item': { color: ui.palette.accents_1 }
+    '& .react-datepicker__time-list': { background: ui.palette.background },
+    '& .react-datepicker__time-list-item': {
+      padding: 2,
+      color: ui.palette.accents_8,
+      '&:hover': { backgroundColor: `${ui.palette.warning} !Important` },
+      '&::before': { display: 'none' }
+    },
+    '& .react-datepicker__time-container .react-datepicker__time .react-datepicker__time-box ul.react-datepicker__time-list li.react-datepicker__time-list-item': {
+      margin: 8,
+      height: 'auto',
+      borderRadius: '0.3rem'
+    },
+    '& .react-datepicker__time-list-item--selected': {
+      background: `${ui.palette.warning} !Important`
+    },
+    '& .react-datepicker__header': {
+      backgroundColor: `${ui.palette.background} !Important`,
+      color: `${ui.palette.accents_8} !Important`
+    },
+    '& .react-datepicker__current-month': {
+      color: `${ui.palette.accents_8} !Important`
+    },
+    '& .react-datepicker__day-name': {
+      color: `${ui.palette.accents_8} !Important`
+    },
+    '& .react-datepicker-time__header': {
+      color: `${ui.palette.accents_8} !Important`
+    }
   }
 }))
 
@@ -31,7 +72,7 @@ export default function DateTimePicker({
       {label && <Text className={classes.label}>{label}</Text>}
       <div className={fullWidth && classes.datePickerWrapper}>
         <DatePicker
-          dateFormat="MMMM d, yyyy h:mm aa"
+          dateFormat="MMMM d, yyyy, h:mm aa"
           showTimeSelect
           timeIntervals={1}
           onChange={onChange}
